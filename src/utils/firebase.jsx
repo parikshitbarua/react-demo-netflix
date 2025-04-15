518a93e6d8899f92d1d5e8ef6449379494b0438b
parikshitbarua <parikshitb04@gmail.com>
2025-04-15 21:13:13 +0530

    remove config

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCqTUrWcZWd9wtwArPgwlYhGzM7sd6ZF_w",
-    authDomain: "netflixgpt-50403.firebaseapp.com",
-    projectId: "netflixgpt-50403",
-    storageBucket: "netflixgpt-50403.firebasestorage.app",
-    messagingSenderId: "133903718756",
-    appId: "1:133903718756:web:f230f5eb892f95154bc271",
-    measurementId: "G-FVT5SCX8E3"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
